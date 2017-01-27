4ccedebacf7f8ea4486de1c373b3c5c3bfe9945f
ouedki <yassineouedki_5@hotmail.com>
2017-01-27 17:13:36 -0600

    hello world servlet

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,25 @@
+
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet(name = "HelloServlet", urlPatterns = "")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        int count = 1;
+        Object counter = req.getParameter(String.valueOf(count));
+        if (counter !=null) {
+            count = Integer.parseInt((String) counter);
+            count++;
+        } else {
+            count = 1;
+        }
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("<h1>Hello, World!</h1>");
+        out.println("<p>Page counter: " + count + "</p>");
     }
 }
