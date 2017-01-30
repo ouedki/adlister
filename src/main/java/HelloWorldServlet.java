f42ab599251ec27db7d3838171cda5dd52248a01
ouedki <yassineouedki_5@hotmail.com>
2017-01-30 15:26:55 -0600

    Added page view counter to hello world

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,6 +1,4 @@
 
-import sun.rmi.rmic.iiop.Type;
-
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -8,27 +6,23 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
+    private int count = 0;
+
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+        count+=1;
         res.setContentType("text/html");
         String name = req.getParameter("name");
-        int count = 0;
-        Object counter = req.getAttribute(String.valueOf(count));
         PrintWriter out = res.getWriter();
 
-        if (name !=null) {
+        if (name != null) {
             out.println("<h1>Hello, " + name + "</h1>");
         } else {
             out.println("<h1>Hello, World!</h1>");
         }
 
-        if (counter !=null) {
-            count = Integer.parseInt((String) counter);
-            count++;
-        } else {
-            count = 1;
-        }
         out.println("<p>Page counter: " + count + "</p>");
     }
 }
+
