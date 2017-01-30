3c2123ad17d1776086a3145829dbc34d029a379d
ouedki <yassineouedki_5@hotmail.com>
2017-01-30 12:30:04 -0600

    completed hello world servlets exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,4 +1,6 @@
 
+import sun.rmi.rmic.iiop.Type;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -6,20 +8,27 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name = "HelloServlet", urlPatterns = "")
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "")
 public class HelloWorldServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
-        int count = 1;
-        Object counter = req.getParameter(String.valueOf(count));
+        res.setContentType("text/html");
+        String name = req.getParameter("name");
+        int count = 0;
+        Object counter = req.getAttribute(String.valueOf(count));
+        PrintWriter out = res.getWriter();
+
+        if (name !=null) {
+            out.println("<h1>Hello, " + name + "</h1>");
+        } else {
+            out.println("<h1>Hello, World!</h1>");
+        }
+
         if (counter !=null) {
             count = Integer.parseInt((String) counter);
             count++;
         } else {
             count = 1;
         }
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
-        out.println("<h1>Hello, World!</h1>");
         out.println("<p>Page counter: " + count + "</p>");
     }
 }
